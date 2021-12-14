7cc7c758cd6d0b35802282e95b6057bcde040152
Alan Dias <alan_bmz@hotmail.com>
2021-12-14 13:31:21 -0300

    fist commit

---
+++ televisao_def.py
@@ -0,0 +1,36 @@
+class Televisao:
+    def __init__(self):
+        self.ligada = False
+        self.canal = 5
+
+    def power(self):
+            if self.ligada:
+                self.ligada = False
+            else:
+                self.ligada = True
+    def aumenta_canal(self):
+        if self.ligada:
+           self.canal +=1
+
+    def diminui_canal(self):
+        if self.ligada:
+           self.canal -=1
+
+print(__name__)
+if __name__ == '__main__':
+
+    televisao = Televisao()
+    print('televisao esta ligada: {}'.format(televisao.ligada))
+    televisao.power()
+    print('televisao esta ligada:  {}'.format(televisao.ligada))
+    televisao.power()
+    print('televisao esta ligada:  {}'.format(televisao.ligada))
+    televisao.power()
+    print('televisao esta ligada: {}'.format(televisao.ligada))
+    print('canal: {}'.format(televisao.canal))
+    televisao.aumenta_canal()
+    televisao.aumenta_canal()
+    televisao.diminui_canal()
+    print('canal: {}'.format(televisao.canal))
+
+
